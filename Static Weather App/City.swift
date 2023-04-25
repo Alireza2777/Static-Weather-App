703e3c8ca1e7f7a1dc7af8cd61588b33bdfd30e8
Ali <alirezamonfared813@gmail.com>
2023-04-25 08:37:11 -0700

    s1

---
+++ Static Weather App/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  Static Weather App
+//
+//  Created by Alireza Karimi on 2023-04-25.
+//
+
+import Foundation
+
+struct City {
+	let name: String
+	let temp: Double
+	let precipitation: Double
+	let icon: String
+	let summary: String
+}
