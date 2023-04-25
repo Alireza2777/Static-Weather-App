548ffb8421c9c0947e24e4065218fbc28296383c
Ali <alirezamonfared813@gmail.com>
2023-04-25 08:23:16 -0700

    Initial Commit

---
+++ Static Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Static Weather App
+//
+//  Created by Alireza Karimi on 2023-04-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
